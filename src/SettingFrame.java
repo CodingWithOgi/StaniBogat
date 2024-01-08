4f429ff72ec0391efe625aa951693c73edb3f773
browsinginthespace <stefanpm20@gmail.com>
2024-01-09 00:27:17 +0200

    added buttons in settings frame

--- src/SettingFrame.java
+++ src/SettingFrame.java
@@ -10,6 +10,7 @@ public class SettingFrame {
         JFrame frame = new JFrame("Settings");
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setSize(1920, 1080);
+        frame.setLayout(null);
 
         Image icon = Toolkit.getDefaultToolkit().getImage("src/images/settings.png");
         frame.setIconImage(icon);
@@ -20,13 +21,53 @@ public class SettingFrame {
         nadpis.setFont(new Font("Times New Roman", 0 ,40));
         frame.add(nadpis);
 
-        JButton btn = new JButton("close settings");
-        btn.addActionListener(new ActionListener() {
+        JButton btnClose = new JButton("close settings");
+        btnClose.setBounds(860, 200, 200, 40);
+        btnClose.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                System.exit(0);
+                frame.dispose();
             }
         });
+        frame.add(btnClose);
         frame.setVisible(true);
+
+        JButton btnMusic = new JButton("music on/off");
+        btnMusic.setBounds(860, 275, 200, 40);
+
+        frame.add(btnMusic);
+        frame.setVisible(true);
+
+
+        JButton btnName = new JButton("change name");
+        btnName.setBounds(860, 350, 200, 40);
+        frame.add(btnName);
+        JTextField fldName = new JTextField("Type in your name here");
+        fldName.setBounds(560, 425, 800, 40);
+        fldName.setVisible(false);
+        frame.add(fldName);
+        JButton btnOk = new JButton("Okay");
+        btnOk.setBounds(860,500,200,40);
+        btnOk.setVisible(false);
+        frame.add(btnOk);
+
+        btnName.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                fldName.setVisible(true);
+                btnOk.setVisible(true);
+            }
+        });
+
+        btnOk.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                frame.dispose();
+            }
+        });
+
+
+
+
     }
 }
